b7533b518016e0b0e0f5c29fe7a319446881306e
jmeade11 <jennifer.meade@general.assemb.ly>
2019-07-18 01:01:03 -0400

    Convert Header component to react-bootstrap Navbar
    Convert the `Header` Component to the React-Bootstrap Navbar
    component.  Replace the home navlink with a link for the
    `/books` route.  Delete the unneeded `Header.scss` file and
    the corresponding import into `Header.js`.

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,37 +1,39 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-
-import './Header.scss'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
 
 const authenticatedOptions = (
   <React.Fragment>
-    <Link to="/change-password">Change Password</Link>
-    <Link to="/sign-out">Sign Out</Link>
+    <Nav.Link href="#change-password">Change Password</Nav.Link>
+    <Nav.Link href="#sign-out">Sign Out</Nav.Link>
   </React.Fragment>
 )
 
 const unauthenticatedOptions = (
   <React.Fragment>
-    <Link to="/sign-up">Sign Up</Link>
-    <Link to="/sign-in">Sign In</Link>
+    <Nav.Link href="#sign-up">Sign Up</Nav.Link>
+    <Nav.Link href="#sign-in">Sign In</Nav.Link>
   </React.Fragment>
 )
 
 const alwaysOptions = (
   <React.Fragment>
-    <Link to="/">Home</Link>
+    <Nav.Link href="#books">Books</Nav.Link>
   </React.Fragment>
 )
 
 const Header = ({ user }) => (
-  <header className="main-header">
-    <h1>Uber, But For Taxis</h1>
-    <nav>
-      { user && <span>Welcome, {user.email}</span>}
-      { user ? authenticatedOptions : unauthenticatedOptions }
-      { alwaysOptions }
-    </nav>
-  </header>
+  <Navbar collapseOnSelect bg="primary" variant="dark" expand="md">
+    <Navbar.Brand href="#home">Book Before You Leap</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="ml-auto">
+        { user && <span className="navbar-text mr-2">Welcome, {user.email}</span>}
+        { alwaysOptions }
+        { user ? authenticatedOptions : unauthenticatedOptions }
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
 )
 
 export default Header
