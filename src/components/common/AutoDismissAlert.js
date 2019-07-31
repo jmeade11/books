bf157ce0f36266db8a477e880077543277b4623f
jmeade11 <jennifer.meade@generalassemb.ly>
2019-07-30 21:53:54 -0400

    Fix the timing on the auto dismiss

--- src/components/common/AutoDismissAlert.js
+++ src/components/common/AutoDismissAlert.js
@@ -15,7 +15,7 @@ class AutoDismissAlert extends React.Component {
   componentDidMount () {
     this.timer = setInterval(() => {
       this.setState({ show: false })
-    }, 15000)
+    }, 5000)
   }
 
   componentWillUnmount () {
